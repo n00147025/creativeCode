35f29176fc2ed0f77aa3816f21a657d3ea5a9c70
n00147025 <44161672+n00147025@users.noreply.github.com>
2018-10-22 13:56:27 +0100

    Updated Comments

--- Colour/exercise06/Step02/step01.js
+++ Colour/exercise06/Step02/step01.js
@@ -13,8 +13,8 @@ function setup() {
   noStroke();
 
   // init with random values
-  for (var i = 0; i < tileCountX; i++) {
-    hueValues[i] = random(360);
+  for (var i = 0; i < tileCountX; i++) {        //creating a random value for HSB
+    hueValues[i] = random(360);                 //inserting the colour values into 3 seperatte arrays
     saturationValues[i] = random(100);
     brightnessValues[i] = random(100);
   }
@@ -32,12 +32,12 @@ function draw() {
   var counter = 0;
 
   // map mouse to grid resolution
-  var currentTileCountX = int(map(mX, 0, width, 1, tileCountX));
-  var currentTileCountY = int(map(mY, 0, height, 1, tileCountY));
-  var tileWidth = width / currentTileCountX;
+  var currentTileCountX = int(map(mX, 0, width, 1, tileCountX));        //mapping the mX to the width of the canvas and from 1 to the number of tiles
+  var currentTileCountY = int(map(mY, 0, height, 1, tileCountY));       //Does the same for the my except with the canvas height
+  var tileWidth = width / currentTileCountX;                            // calculates the tile height and width 
   var tileHeight = height / currentTileCountY;
 
-  for (var gridY = 0; gridY < tileCountY; gridY++) {
+  for (var gridY = 0; gridY < tileCountY; gridY++) {                    
     for (var gridX = 0; gridX < tileCountX; gridX++) {
       var posX = tileWidth * gridX;
       var posY = tileHeight * gridY;
